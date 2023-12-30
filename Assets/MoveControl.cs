3a36be26884d0059c8a6fc756cafad3deddee063
Griffin <gcol0008@student.monash.edu>
2023-12-30 12:18:43 +1100

    Added footstep noises

--- Assets/MoveControl.cs
+++ Assets/MoveControl.cs
@@ -8,10 +8,19 @@ public class MoveControl : MonoBehaviour
     Rigidbody rigidbody;
     float speed = 4f;
 
+    [SerializeField]
+    AudioSource walkFootsteps;
+    [SerializeField]
+    AudioSource runFootsteps;
+
+    AudioSource curFootsteps;
+
+    float walkVolume = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -19,6 +28,34 @@ public class MoveControl : MonoBehaviour
     void FixedUpdate()
     {
         UpdateMove();
+    }
+
+
+    void StartFootsteps()
+    {
+        curFootsteps = Input.GetKey(KeyCode.LeftShift) ? runFootsteps : walkFootsteps;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            walkFootsteps.volume = 0;
+            walkFootsteps.Stop();
+        }
+        else
+        {
+            runFootsteps.volume = 0;
+            runFootsteps.Stop();
+        }
+        if (!curFootsteps.isPlaying)
+        {
+            curFootsteps.Play();
+        }
+        curFootsteps.volume = Mathf.Lerp(curFootsteps.volume, walkVolume, 0.8f);
+    }
+
+    void StopFootsteps()
+    {
+        runFootsteps.volume = 0;
+        curFootsteps = walkFootsteps;
+        curFootsteps.volume = Mathf.Lerp(curFootsteps.volume, 0, 0.8f);
     }
 
 
@@ -33,7 +70,16 @@ public class MoveControl : MonoBehaviour
             movedir.Normalize();
         }
 
-        movedir *= speed * (Input.GetKey(KeyCode.LeftShift) ? 1.3f : 1);
+        if (movedir.sqrMagnitude == 0)
+        {
+            StopFootsteps();
+        }
+        else
+        {
+            StartFootsteps();
+        }
+
+        movedir *= speed * (Input.GetKey(KeyCode.LeftShift) ? 1.6f : 1);
 
         rigidbody.MovePosition(transform.position + movedir * Time.deltaTime);
 
